63d9cab59681e961c718c393a379dc53c328ba07
descomkraneoz <descom666999@gmail.com>
2019-09-23 12:18:13 +0200

    final commit

--- src/Ejemplo1.java
+++ src/Ejemplo1.java
@@ -6,20 +6,20 @@ public class Ejemplo1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try 
-        { 
-            // Se lanza el ejecutable. 
-            Process p=Runtime.getRuntime().exec ("notepad.exe"); 
+		try
+        {
+            // Se lanza el ejecutable.
+            Process p=Runtime.getRuntime().exec ("notepad.exe");
             // Esperamos a que termine el ejecutable
-            p.waitFor(); 
+            p.waitFor();
             // Mostramos por pantalla un mensaje
-            System.out.println("El proceso ha terminado.");         
-        }  
-        catch (Exception e) 
-        { 
+            System.out.println("El proceso ha terminado.");
+        }
+        catch (Exception e)
+        {
             // Excepciones si hay algún problema al arrancar el ejecutable o al leer su salida.*/
-             e.printStackTrace(); 
-        } 
+             e.printStackTrace();
+        }
 	}
 
 }
